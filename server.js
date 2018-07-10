b93975e1fcc9ade846a7fa9cd351abc6cb6d3533
Nicole Houston <nicole.houston@cox.net>
2018-07-09 21:38:52 -0700

    Articles from Nintendo news on IGN are showing up with buttons for notes. Need to still check if article already exists in database and be able to create notes

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var logger = require("morgan");
 var mongoose = require("mongoose");
 
 // Our scraping tools
-var request = require("request");
+var axios = require("axios");
 var cheerio = require("cheerio");
 
 // Require all models
@@ -15,10 +14,14 @@ var PORT = process.env.PORT || 3000;
 // Initialize Express
 var app = express();
 
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 // Configure middleware
 
-// Use morgan logger for logging requests
-app.use(logger("dev"));
 // Use body-parser for handling form submissions
 app.use(bodyParser.urlencoded({ extended: true }));
 // Use express.static to serve the public folder as a static directory
@@ -32,3 +35,61 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/mongoHea
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
+
+// Routes
+
+// A GET route for scraping nintendo articles
+app.get("/", function(req, res) {
+  // First, we grab the body of the html with request
+  axios.get("http://www.ign.com/nintendo").then(function(response) {
+    // Then, we load that into cheerio and save it to $ for a shorthand selector
+    var $ = cheerio.load(response.data);
+
+    // Now, we grab every blog-item from the IGN website
+    $(".listElmnt-blogItem").each(function(i, element) {
+      // Save an empty result object
+      var result = {};
+
+      // Add the text and href of every link, and save them as properties of the result object
+      result.title = $(this)
+        .children("a")
+        .text();
+      result.summary = $(this)
+        .children("p").
+        text();
+      result.link = $(this)
+        .children("a")
+        .attr("href");
+
+      // Create a new Article using the `result` object built from scraping
+        db.Article.create(result)
+        .then(function(dbArticle) {
+          var hbsObject = {
+            article: dbArticle
+          };
+          result.render("index", hbsObject);
+        })
+        .catch(function(err) {
+          // If an error occurred, send it to the client
+          return res.json(err);
+        });
+    });
+  });
+  db.Article.find({})
+    .then(function(dbArticle) {
+      var hbsObject = {
+        article: dbArticle
+      };
+      res.render("index", hbsObject);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+  // Start the server
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT + "!");
+});
+  
